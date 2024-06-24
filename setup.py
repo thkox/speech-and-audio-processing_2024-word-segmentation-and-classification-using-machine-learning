79f6f20d70f7557f46dd014ae0c86362db595ccc
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-24 22:59:15 +0300

    - update the setup to take the uninstallation parameter of the env

--- setup.py
+++ setup.py
@@ -4,12 +4,24 @@ import subprocess
 
 
 def create_conda_env():
-    # Define the command to create the conda environment
-    command = "conda env create -f environment.yml"
+    env_name = "speech-and-audio-processing"
+
+    # Define the command to remove the conda environment if it exists
+    remove_command = f"conda env remove --name {env_name}"
 
     try:
-        # Run the command
-        subprocess.check_call(command, shell=True)
+        # Run the remove command
+        subprocess.check_call(remove_command, shell=True)
+        print("Existing conda environment removed successfully.")
+    except subprocess.CalledProcessError:
+        print("No existing conda environment to remove.")
+
+    # Define the command to create the conda environment
+    create_command = "conda env create -f environment.yml"
+
+    try:
+        # Run the create command
+        subprocess.check_call(create_command, shell=True)
         print("Conda environment created successfully.")
     except subprocess.CalledProcessError as e:
         print("Failed to create conda environment.")
