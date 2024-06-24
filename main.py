b7539343d3fee85b27e10d7b73ab0fcef43169cc
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-24 23:02:14 +0300

    - add steps def for training

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import create_datasets_for_app as cda
+import classifiers.feature_extraction as fe
+import classifiers.model_trainning as mt
 
 # Create the datasets that are necessary for the app
-cda.create_datasets()
+# cda.create_datasets()
+
+# Feature extraction of both background and foreground datasets
+fe.extract_features()
 
 # Train the classifiers
+# mt.train_classifiers()
