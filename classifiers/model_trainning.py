0576fdaf6d5e54b53d0202439c36788f791591ad
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-25 21:46:44 +0300

    - remove the output dir - add dummy file in output directory

--- classifiers/model_trainning.py
+++ classifiers/model_trainning.py
@@ -1,8 +1,8 @@
 import classifiers.svm as svm
 
 
-def train_classifiers(features, labels, output_dir):
+def train_classifiers(features, labels):
     # Train SVM classifier
-    svm_classifier = svm.train_svm(features, labels, output_dir)
+    svm_classifier = svm.train_svm(features, labels)
 
     return svm_classifier
