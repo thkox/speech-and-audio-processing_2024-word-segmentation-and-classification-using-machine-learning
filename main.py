59f161da921b0cf62bd6f3d7490c69bcc1bbf03e
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-06-28 03:54:42 +0300

    - changes

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import os
 from scipy.ndimage import median_filter
 from classifiers import mlp, rnn, svm, least_squares as ls
 
-# Create the datasets that are necessary for the app
-cda.create_datasets()
-
-# Feature extraction of both background and foreground datasets
-fe.extract_features(shuffle_data=True, show_plots=False)  # to create and save the features
-
-# Train the classifiers
-svm.train()
-mlp.train()
-rnn.train()
-ls.train()
+# # Create the datasets that are necessary for the app
+# cda.create_datasets()
+#
+# # Feature extraction of both background and foreground datasets
+# fe.extract_features(shuffle_data=True, show_plots=False)  # to create and save the features
+#
+# # Train the classifiers
+# svm.train()
+# mlp.train()
+# rnn.train()
+# ls.train()
 
 # Load the trained models
 svm_model = svm.load_model()
@@ -24,8 +24,7 @@ rnn_model = rnn.load_model()
 ls_model = ls.load_model()
 
 # check a test file to predict the labels
-# test_file = '/home/theo/Downloads/LibriSpeech/test-clean/2094/142345/2094-142345-0008.flac'
-test_file = 'files/datasets/test/test_2.mp3'
+test_file = '/home/theo/Downloads/LibriSpeech/test-clean/2094/142345/2094-142345-0008.flac'
 
 # get the labels from the test file
 # labels = fe.webrtc_vad_speech_detection(test_file)
@@ -36,8 +35,7 @@ _, features, sample_rate, audio = fe.load_and_extract_features(test_file, show_p
 hop_length = fe.HOP_LENGTH
 frame_rate = sample_rate / hop_length
 
-# Median Filter
-L = 3
+L = 5  # Window size for median filter
 
 # Predict the labels using the trained models
 svm_predictions = svm.predict(features)
@@ -76,10 +74,10 @@ def show_predictions(predictions, frame_rate, title):
 
 
 # Show the predictions
-show_predictions(svm_predictions, frame_rate, "SVM predictions")
-show_predictions(mlp_predictions, frame_rate, "MLP predictions")
-show_predictions(rnn_predictions, frame_rate, "RNN predictions")
-show_predictions(ls_predictions, frame_rate, "Least Squares predictions")
-show_predictions(majority_voting, frame_rate, "Majority voting predictions")
+show_predictions(svm_predictions_median, frame_rate, "SVM predictions")
+show_predictions(mlp_predictions_median, frame_rate, "MLP predictions")
+show_predictions(rnn_predictions_median, frame_rate, "RNN predictions")
+show_predictions(ls_predictions_median, frame_rate, "Least Squares predictions")
+show_predictions(majority_voting_median, frame_rate, "Majority voting predictions")
 
 print("=====================================")
